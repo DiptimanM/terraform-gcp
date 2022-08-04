4af566f3116260a53b805ef3409c6b5ad2864318
DiptimanM <110095081+DiptimanM@users.noreply.github.com>
2022-08-05 02:06:54 +0530

    Update folder.tf

--- folder.tf
+++ folder.tf
@@ -1,4 +1,9 @@
+resource "google_folder" "ams_labs" {
+  display_name = "AMS Labs"
+  parent       = "organizations/707556800543"
+}
+
 resource "google_folder" "TMN" {
   display_name = "TMN"
-  parent       = organizations/707556800543/510816707782
+  parent       = google_folder.ams_labs
 }
