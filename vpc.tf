73d0a839c13af6ac6c468893ab33494e9e158ff3
DiptimanM <110095081+DiptimanM@users.noreply.github.com>
2022-08-05 02:42:00 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
 resource "google_compute_network" "terraform_host_network" {
-  project                 = "terraform-project"
+  project                 = "terraform-project-344023"
   name                    = "terraform-host-network"
 }
