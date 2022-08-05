b60ddf12ac27b505b74a891537a235b491e5734e
Mukherjee <Diptiman.Mukherjee@Virtustream.com>
2022-08-05 12:34:18 +0530

    Third Commit

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,5 @@
 resource "google_compute_network" "terraform_host_network" {
   project                 = var.projectName
   name                    = var.vpcName
+  auto_create_subnetworks = false
 }
